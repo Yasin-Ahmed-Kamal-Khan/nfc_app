b992df1d19105fdd2eaa513fa2fd0378a446fb20
Yasin Khan <yak23@ic.ac.uk>
2025-07-20 13:32:17 +0100

    misc: remove template tabs

--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -2,5 +2,3 @@ export 'enter_data_tab.dart';
 export 'nfc_reader_tab.dart';
 export 'json_transmit_tab.dart';
 export 'write_to_nfc_card.dart';
-export 'template_tab_3.dart';
-export 'template_tab_4.dart';
