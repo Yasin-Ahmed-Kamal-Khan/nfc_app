b8f1378d933dc3772a0537f6fccf3f8449d6d413
Bitmapp3r <tausif_samad@yahoo.com>
2025-07-20 20:21:15 +0100

    Refactor file names

--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -1,4 +1,4 @@
 export 'enter_data_tab.dart';
-export 'nfc_qr_reader_tab.dart';
-export 'json_transmit_tab.dart';
-export 'json_qr_tab.dart';
+export 'nfc_qr_scanner_tab.dart';
+export 'nfc_transfer_tab.dart';
+export 'json_qr_display_tab.dart';
